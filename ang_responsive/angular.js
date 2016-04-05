99efd76b74a062fdc3fcd1b8fd58ddccd59010f9
Trisha Meyer <tmeye62@gmail.com>
2016-04-05 14:07:55 -0700

    added banner images and started making container for main content, also switched banner images based on page

--- ang_responsive/angular.js
+++ ang_responsive/angular.js
@@ -10,26 +10,36 @@ routerApp.config(function ($stateProvider, $urlRouterProvider) {
 
         .state('welcome', {
             url: '/welcome',
-            templateUrl: 'templates/home.html'
-            //controller: 'logInController'
+            templateUrl: 'templates/home.html',
+            controller: 'welcomeController'
         })
 
         .state('our_macarons', {
             url: '/our_macarons',
-            templateUrl: 'templates/our_macarons.html'
-            //controller: 'signupController'
+            templateUrl: 'templates/our_macarons.html',
+            controller: 'macaronsController'
         })
 
         .state('gifts_parties', {
             url: '/gifts_parties',
-            templateUrl: 'templates/gifts_parties.html'
-            //controller: 'newsFeedController'
+            templateUrl: 'templates/gifts_parties.html',
+            controller: 'giftsController'
         })
 
         .state('contact', {
             url: '/contact',
-            templateUrl: 'templates/contact.html'
-            //controller: 'profilePageController'
+            templateUrl: 'templates/contact.html',
+            controller: 'contactController'
         })
 
+});
+
+routerApp.controller('welcomeController', function($scope){
+    $scope.banner = 'assets/images/welcome-image.png';
+}).controller('macaronsController', function($scope){
+    $scope.banner = 'assets/images/our-macarons-image.png';
+}).controller('giftsController', function($scope){
+    $scope.banner = 'assets/images/gifts-parties-image.png';
+}).controller('contactController', function($scope){
+    $scope.banner = 'assets/images/contact-image.png';
 });
